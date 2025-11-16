11cc8d909e79e02e1f34a8fc762e4c85f106d6eb
Ростислав Грачев <gracevrostislav5@gmail.com>
2025-11-16 13:40:52 +0700

    Добавил правильную обработку исключений при обавлении книг

--- RestApiDjango/RestApiBooks/models.py
+++ RestApiDjango/RestApiBooks/models.py
@@ -1,20 +1,23 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     patronymic = models.CharField(max_length=100, blank=True)
     biography = models.TextField()
+
     class Meta:
         unique_together = ['first_name', 'last_name', 'patronymic']
+
     def __str__(self):
         if self.patronymic:
             return f"{self.last_name} {self.first_name} {self.patronymic}"
         return f"{self.last_name} {self.first_name}"
 
-class Book(models.Model):
 
+class Book(models.Model):
     CATEGORY_CHOICES = [
         ('fiction', 'Художественная литература'),
         ('textbook', 'Учебник'),
@@ -26,11 +29,8 @@ class Book(models.Model):
     category = models.CharField(max_length=100, choices=CATEGORY_CHOICES)
     publisher = models.CharField(max_length=100)
     image = models.ImageField(upload_to='book_covers/')
-    text_file = models.FileField( upload_to='book_texts/')
+    text_file = models.FileField(upload_to='book_texts/')
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = ['title', 'author', 'year_release', 'publisher']
 
     def __str__(self):
         return f"{self.title} - {self.author} ({self.year_release})"
--- RestApiDjango/RestApiBooks/serializers.py
+++ RestApiDjango/RestApiBooks/serializers.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 
 from .models import Book, Author
@@ -6,6 +7,38 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields =  '__all__'
+
+    def validate(self, data):
+        data = super().validate(data)
+
+        instance = self.instance
+        title = data.get('title')
+        author = data.get('author')
+        category = data.get('category')
+        year_release = data.get('year_release')
+        publisher = data.get('publisher')
+
+        existing_books = Book.objects.filter(title=title, author=author)
+
+        if instance:
+            existing_books = existing_books.exclude(pk=instance.pk)
+
+        if existing_books.exists():
+            if category == 'textbook':
+                if existing_books.filter(year_release=year_release).exists():
+                    existing_years = existing_books.values_list('year_release', flat=True)
+                    raise serializers.ValidationError({
+                        'year_release': f'Учебник с таким годом выпуска уже существует. ' f'Существующие годы выпуска: {list(existing_years)}. ' 'Для учебников разрешены только переиздания с новым годом выпуска.'
+                    })
+
+            elif category == 'fiction':
+                if existing_books.filter(publisher=publisher).exists():
+                    existing_publishers = existing_books.values_list('publisher', flat=True)
+                    raise serializers.ValidationError({
+                        'publisher': f'Художественное произведение от этого издательства уже существует. ' f'Существующие издательства: {list(existing_publishers)}. ' 'Для художественной литературы разрешены книги от разных издательств.'
+                    })
+
+        return data
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
